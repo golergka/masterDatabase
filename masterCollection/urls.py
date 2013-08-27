e0051d4aac62a1857be6f02d9a6b0873e6c54d91
Max Yankov <golergka@gmail.com>
2013-08-27 11:01:29 +0100

    Master view stub

--- masterCollection/urls.py
+++ masterCollection/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from masterCollection import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$',                             views.index,  name='index'),
+    url(r'^master/(?P<master_id>\d+)/$',   views.master, name='master'),
 )
--- masterCollection/views.py
+++ masterCollection/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("TODO index")
+
+def master(request, master_id):
+    return HttpResponse("TODO master view %s" % master_id)
