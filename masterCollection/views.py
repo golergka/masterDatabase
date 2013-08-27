f8623e898da377176c89696d18f7de3cebccab34
Max Yankov <golergka@gmail.com>
2013-08-27 12:35:15 +0100

    Simplest index view

--- masterCollection/views.py
+++ masterCollection/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from masterCollection.models import Master
 
 def index(request):
-    return HttpResponse("TODO index")
+    master_list = Master.objects.iterator()
+    template = loader.get_template('masterCollection/index.html')
+    context = RequestContext(
+        request,
+        {
+            'master_list': master_list
+        }
+    )
+
+    return HttpResponse(template.render(context))
 
 def master(request, master_id):
     return HttpResponse("TODO master view %s" % master_id)
