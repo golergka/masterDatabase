9daa561ceb1002fa3ffea368be2fb94bb62e057c
Max Yankov <golergka@gmail.com>
2013-08-27 12:38:08 +0100

    Made list view a bit simpler

--- masterCollection/views.py
+++ masterCollection/views.py
@@ -1,19 +1,12 @@
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render
 
 from masterCollection.models import Master
 
 def index(request):
     master_list = Master.objects.iterator()
-    template = loader.get_template('masterCollection/index.html')
-    context = RequestContext(
-        request,
-        {
-            'master_list': master_list
-        }
-    )
-
-    return HttpResponse(template.render(context))
+    context = { 'master_list': master_list }
+    return render(request, 'masterCollection/index.html', context)
 
 def master(request, master_id):
     return HttpResponse("TODO master view %s" % master_id)
